8f49f3d5d70051fd6743216aea5c95b95549fe34
SamujjalDas <samujjal.das97@gmail.com>
2021-06-06 00:51:03 +0530

    Login functionality

--- src/app/bugservice.service.ts
+++ src/app/bugservice.service.ts
@@ -2,40 +2,63 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bug } from './bug';
+import { UserDetails } from './userdetails';
 
 import { HttpHeaders } from '@angular/common/http';
- 
+
 //import { Bug } from './bug'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BugserviceService {
-  private apiServerUrl ='http://127.0.0.1:8080';
-  private _bugId ;
-  constructor(private http:HttpClient) { }
+  private apiServerUrl = 'http://127.0.0.1:8080';
+  private _bugId;
+  username = 'dummy';
+  password = 'dummy';
 
+  constructor(private http: HttpClient) { }
 
+  //headers= new HttpHeaders();
 
-  public headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8',
-  'Access-Control-Allow-Origin': 'http://localhost:8080', 'Access-Control-Allow-Credentials': 'true'});
+  //const headers = new HttpHeaders({Authorization: 'Basic '+btoa(this.username+":"+this.password)});
+  //headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //headers.append('Access-Control-Allow-Credentials', 'true');
 
+  //headers= new HttpHeaders()
+  //.set('Authorization', 'Basic '+(this.username+":"+this.password))
+  //  .set('content-type', 'application/json')
+  // .set('Access-Control-Allow-Origin', '*')
+  // .set('Access-Control-Allow-Credentials', 'true')
+  ;
 
-  public getBug(bugId : number) : Observable<Bug[]>{
+  public login(userDetails: UserDetails) {
+    this.username = userDetails.username;
+    this.password = userDetails.password;
+    console.log(this.password);
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(this.username + ":" + this.password) });
+    console.log(headers);
+    return this.http.get<Bug>(`${this.apiServerUrl}/loginApp`, { headers, responseType: 'text' as 'json' });
+  }
+
+  public getBug(bugId: number): Observable<Bug[]> {
     console.log(bugId);
     console.log(this.apiServerUrl);
     var obj = bugId;
     this._bugId = Object.values(bugId);
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(this.username + ":" + this.password) });
     //return this.http.get<Bug[]>(`${this.apiServerUrl}/search/${bugId}`);
-    return this.http.get<Bug[]>(`${this.apiServerUrl}/search/`+`${this._bugId}`);
+    return this.http.get<Bug[]>(`${this.apiServerUrl}/search/` + `${this._bugId}`, { headers });
   }
 
-  public createBug(bug : Bug) : Observable<Bug>{
+  public createBug(bug: Bug): Observable<Bug> {
     console.log(bug);
-    return this.http.post<Bug>(`${this.apiServerUrl}/createBug`,bug, {headers: this.headers});
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(this.username + ":" + this.password) });
+    return this.http.post<Bug>(`${this.apiServerUrl}/createBug`, bug, { headers });
   }
 
-  public deleteBug(bugId : number) : Observable<void>{
-    return this.http.delete<void>(`${this.apiServerUrl}/deleteBug/${bugId}`);
+  public deleteBug(bugId: number): Observable<void> {
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(this.username + ":" + this.password) });
+    return this.http.delete<void>(`${this.apiServerUrl}/deleteBug/${bugId}`, { headers });
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { BugserviceService } from '../bugservice.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +11,28 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  msg: any;
+  constructor(private router: Router, private service: BugserviceService) { }
 
   ngOnInit(): void {
   }
-  public loginNow(){
-    console.log("logged in");
-    this.router.navigate(['/search']);
+  // public loginNow(addForm: NgForm): void{
+  //   console.log("logged in");
+  //   console.log(addForm.value);
+  //   this.router.navigate(['/search']);
 
+  // }
+
+  public loginNow(addForm: NgForm): void {
+    this.service.login(addForm.value).subscribe((data) => {
+      console.log(data);
+      this.msg = data;
+      this.router.navigate(['/search']);
+    },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
 }
